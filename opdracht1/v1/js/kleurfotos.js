6760bef4900e6c1ff5f7ca611ee486bfa23eb1e4
Desiree1997 <desireevandam97@gmail.com>
2019-04-24 08:23:29 +0200

    Poging tot javascript

---
+++ opdracht1/v1/js/kleurfotos.js
@@ -0,0 +1,22 @@
+var kleur = document.querySelector("body > header > section > label:nth-child(4) > input[type="checkbox"]");
+
+function showPlaatje() {
+
+    var laatZien = document.querySelector(".Paars");
+
+    document.laatZien.display = "visible";
+}
+kleur.addEventListener("click", showPlaatje);
+
+
+
+/*
+var kleur = document.querySelector("value="Paars"");
+
+function showPlaatje() {
+
+    var verander = document.querySelector(".laatZien");
+
+    document.getElementsByName('Paars').style.display = "block";
+}
+kleur.addEventListener("click", showPlaatje);
