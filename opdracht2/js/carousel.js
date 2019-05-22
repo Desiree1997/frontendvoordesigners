79688a076bd6c84356a8cb79d281300aceeca0df
Desiree1997 <desireevandam97@gmail.com>
2019-05-22 08:31:41 +0200

    met keys erbij

--- opdracht2/js/carousel.js
+++ opdracht2/js/carousel.js
@@ -1,4 +1,4 @@
-var knop = document.querySelector("body > button");
+var knop = document.querySelector("button");
 
 function wisselPlaatje() {
 
@@ -8,3 +8,38 @@ function wisselPlaatje() {
 }
 
 knop.addEventListener("click", wisselPlaatje);
+
+
+
+function volgende(e) {
+
+    switch (e.keyCode) { //met keycode kijkt hij welk getal hij van het keyboard moet pakken
+        case 39: // 39 is knop pijl naar rechts
+
+            var foto = document.querySelector("body > div > img:nth-child(2)");
+            foto.classList.add('onscreen');
+
+        break;
+
+        default:
+            break;
+    }
+} // 39 is pijl naar rechts, als je > indrukt komt het volgende plaatje in beeld vliegen
+document.addEventListener("keydown", volgende);
+
+
+function vorige(e) {
+
+    switch (e.keyCode) { //met keycode kijkt hij welk getal hij van het keyboard moet pakken
+        case 37: // 37 is knop pijl naar links
+
+            var foto = document.querySelector("body > div > img:nth-child(2)");
+            foto.classList.remove('onscreen');
+
+        break;
+
+        default:
+            break;
+    }
+} // 37 is pijl naar links, als je < indrukt komt het volgende plaatje in beeld vliegen
+document.addEventListener("keydown", vorige);
