fd76fe232b77b99a07cfc085b27308d988b8006e
Desiree1997 <desireevandam97@gmail.com>
2019-05-22 14:01:05 +0200

    Helemaal mooi

--- opdracht2/js/carousel.js
+++ opdracht2/js/carousel.js
@@ -1,13 +1,25 @@
-var knop = document.querySelector("button");
+var knop = document.querySelector("body > button:nth-child(3)");
+
+function wisselPlaatjeEen() {
+
+    var foto = document.querySelector("body > div > img:nth-child(2)");
+    foto.classList.remove('onscreen');
+
+}
+knop.addEventListener("click", wisselPlaatjeEen);
+
+
+
+var knop = document.querySelector("body > button:nth-child(4)");
 
 function wisselPlaatje() {
 
     var foto = document.querySelector("body > div > img:nth-child(2)");
     foto.classList.add('onscreen');
-
 }
-
 knop.addEventListener("click", wisselPlaatje);
+
+
 
 
 
@@ -27,6 +39,22 @@ function volgende(e) {
 } // 39 is pijl naar rechts, als je > indrukt komt het volgende plaatje in beeld vliegen
 document.addEventListener("keydown", volgende);
 
+function volgendeTwee(e) {
+
+    switch (e.keyCode) { //met keycode kijkt hij welk getal hij van het keyboard moet pakken
+        case 40: // 39 is knop pijl naar rechts
+
+            var foto = document.querySelector("body > div > img:nth-child(2)");
+            foto.classList.add('onscreen');
+
+        break;
+
+        default:
+            break;
+    }
+} // 39 is pijl naar rechts, als je > indrukt komt het volgende plaatje in beeld vliegen
+document.addEventListener("keydown", volgendeTwee);
+
 
 function vorige(e) {
 
@@ -43,3 +71,19 @@ function vorige(e) {
     }
 } // 37 is pijl naar links, als je < indrukt komt het volgende plaatje in beeld vliegen
 document.addEventListener("keydown", vorige);
+
+function vorigeTwee(e) {
+
+    switch (e.keyCode) { //met keycode kijkt hij welk getal hij van het keyboard moet pakken
+        case 38: // 37 is knop pijl naar links
+
+            var foto = document.querySelector("body > div > img:nth-child(2)");
+            foto.classList.remove('onscreen');
+
+        break;
+
+        default:
+            break;
+    }
+} // 37 is pijl naar links, als je < indrukt komt het volgende plaatje in beeld vliegen
+document.addEventListener("keydown", vorigeTwee);
