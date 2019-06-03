32d87a90b893d6459fa40d9df9f3f24864e8fac6
Desiree1997 <desireevandam97@gmail.com>
2019-06-03 11:30:51 +0200

    Basis

--- opdracht3/v1/script.js
+++ opdracht3/v1/script.js
@@ -12,32 +12,68 @@ request.send();
 request.onload = function () {
     var movies = request.response;
     console.log(movies[0].title);
+    afbeelding(movies[0]);
     populateHeader(movies[0]);
-    discription(movies[0]);
     genres(movies[0]);
-    showFilms(movies[0]);
+    discription(movies[0]);
+    recenties(movies[0]);
+
+    afbeelding(movies[1]);
+    populateHeader(movies[1]);
+    genres(movies[1]);
+    discription(movies[1]);
+    recenties(movies[1]);
+
+    afbeelding(movies[2]);
+    populateHeader(movies[2]);
+    genres(movies[2]);
+    discription(movies[2]);
+    recenties(movies[2]);
+
+    afbeelding(movies[3]);
+    populateHeader(movies[3]);
+    genres(movies[3]);
+    discription(movies[3]);
+    recenties(movies[3]);
+
+    afbeelding(movies[4]);
+    populateHeader(movies[4]);
+    genres(movies[4]);
+    discription(movies[4]);
+    recenties(movies[4]);
+
+    afbeelding(movies[5]);
+    populateHeader(movies[5]);
+    genres(movies[5]);
+    discription(movies[5]);
+    recenties(movies[5]);
 }
 
+function afbeelding(jsonObj) {
+    var myImg = document.createElement('img');
+    myImg.textContent = jsonObj['cover'];
+    section.appendChild(myImg);
+}
 
 function populateHeader(jsonObj) {
     var myH2 = document.createElement('h2');
     myH2.textContent = jsonObj['title'];
-    header.appendChild(myH2);
-}
-
-function discription(jsonObj) {
-    var myp = document.createElement('p');
-    myp.textContent = jsonObj['plot'];
-    header.appendChild(myp);
+    section.appendChild(myH2);
 }
 
 function genres(jsonObj) {
     var myp = document.createElement('p');
     myp.textContent = 'Genre: ' + jsonObj['genres'];
-    header.appendChild(myp);
+    section.appendChild(myp);
 }
 
-function showFilms(jsonObj) {
+function discription(jsonObj) {
+    var myp = document.createElement('p');
+    myp.textContent = jsonObj['plot'];
+    section.appendChild(myp);
+}
+
+function recenties(jsonObj) {
     var recentie = jsonObj['reviews'];
 
     for (var i = 0; i < recentie.length; i++) {
